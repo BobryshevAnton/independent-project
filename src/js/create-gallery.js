031c995ebb4cb00ce62009e27b0c79f62447e88a
Anton Bobryshev <101340267+BobryshevAnton@users.noreply.github.com>
2023-01-04 22:16:07 +0100

    modal work complited

--- src/js/create-gallery.js
+++ src/js/create-gallery.js
@@ -1,23 +1,29 @@
 import SearchApi from './searchApi';
 import createGanres from './create-ganres';
-
+import modalFilm from './modal-film';
 const searchApi = new SearchApi();
 const indexGallery = document.querySelector('.gallery-container');
+// const modalOpen = document.querySelector('.gallery-box');
 
 export function createGallery({ results }) {
+  indexGallery.innerHTML = '';
+
   const markup = results
     .map(({ poster_path, title, release_date, vote_average, genre_ids }) => {
       let emptyImg = 'https://i.ibb.co/JyBCdzw/sinema-empty.jpg';
       let baseImg = `https://image.tmdb.org/t/p/w500${poster_path}`;
-      const baseTitle = `${title}`;
+      let baseTitle = `${title}`;
       const baseDate = `${release_date.slice(0, 4)}`;
-
       let ganresTitle = createGanres(genre_ids);
 
       if (!poster_path) {
         baseImg = emptyImg;
       }
-
+      if (baseTitle.length > 25) {
+        baseTitle = baseTitle.slice(0, 30) + '...';
+      } else {
+        baseTitle;
+      }
       if (ganresTitle.length > 2) {
         ganresTitle = ganresTitle.slice(0, 2).join(', ') + ', Other';
       } else {
@@ -45,4 +51,77 @@ export function createGallery({ results }) {
     .join('');
 
   indexGallery.insertAdjacentHTML('beforeend', markup);
+  modalFilm({ results });
+  //=== elem modal
+  //   const modalCont = document.querySelector('.modal-block');
+  //   const modal = document.querySelector('[data-modal]');
+
+  //   const filmMass = [...results];
+  //   console.log(filmMass);
+
+  //   indexGallery.addEventListener('click', handleClick);
+  //   function handleClick(evt) {
+  //     modal.classList.toggle('is-hidden');
+
+  //     filmMass.map(elem => {
+  //       if (elem.original_title === evt.target.alt) {
+  //         let ganresTitle = createGanres(elem.genre_ids);
+  //         if (ganresTitle.length > 2) {
+  //           ganresTitle = ganresTitle.slice(0, 2).join(', ') + ', Other';
+  //         } else {
+  //           ganresTitle = ganresTitle.join(', ');
+  //         }
+  //         let baseImgFilmModal = `https://image.tmdb.org/t/p/w500${elem.poster_path}`;
+  //         const markupModal = `
+
+  //       <div class="modal-films">
+  //         <img
+  //           class="modal-pictures"
+  //           src="${baseImgFilmModal}"
+  //           alt="${elem.original_title}"
+  //           width="375px"
+  //         />
+  //       </div>
+  //       <div class="modal-info">
+  //         <h2 class="modal-title">${elem.original_title}</h2>
+  //         <table class="modal-info__film">
+  //           <tr>
+  //             <td class="modal-info__left">Vote / Votes</td>
+  //             <td class="modal-info__right">
+  //               <span class="modal-span">${elem.vote_average}</span>/${elem.vote_count}
+  //             </td>
+  //           </tr>
+  //           <tr>
+  //             <td class="modal-info__left">Popularity</td>
+  //             <td class="modal-info__right">${elem.popularity}</td>
+  //           </tr>
+  //           <tr>
+  //             <td class="modal-info__left">Original Title</td>
+  //             <td class="modal-info__right">${elem.original_title}</td>
+  //           </tr>
+  //           <tr>
+  //             <td class="modal-info__left">Genre</td>
+  //             <td class="modal-info__right">${ganresTitle}</td>
+  //           </tr>
+  //         </table>
+
+  //         <div class="modal-bottom">
+  //           <h2 class="modal-bottom__title">About</h2>
+  //           <p class="modal-bottom__text">
+  //             ${elem.overview}
+  //           </p>
+  //         </div>
+  //         <div class="modal-button__section">
+  //           <button class="modal__btn-libryary" type="button">
+  //             add to Watched
+  //           </button>
+  //           <button class="modal__btn-libryary" type="button">queue</button>
+  //         </div>
+  //       </div>
+  //     `;
+  //         modalCont.innerHTML = '';
+  //         return modalCont.insertAdjacentHTML('afterbegin', markupModal);
+  //       }
+  //     });
+  //   }
 }
--- src/js/search.js
+++ src/js/search.js
@@ -2,32 +2,42 @@ import SearchApi from './searchApi';
 import { createGallery } from './create-gallery';
 
 const form = document.querySelector('.header__form-search');
-
+const indexGallery = document.querySelector('.gallery-container');
 let searchFilm = '';
 let page = 1;
 
 form.addEventListener('submit', handleSubmit);
 
-// const anyText = document.querySelector('.header-text');
-// anyText.classList.add('header-txt');
-
+const anyText = document.querySelector('.text-empty');
+anyText.classList.add('hiden');
+anyText.textContent =
+  'Search result not successful. Enter the correct movie name...';
 const searchApi = new SearchApi();
+
+startPage();
 
 function handleSubmit(event) {
   event.preventDefault();
   const {
     elements: { search },
   } = event.currentTarget;
-
   searchFilm = search.value;
-
   page += 1;
+
+  if (searchFilm === '') {
+    anyText.classList.remove('hiden');
+    return;
+  } else {
+    anyText.classList.add('hiden');
+  }
+
   searchApi.getSearchMovie(searchFilm, page).then(data => {
-    if (!data.total_results) {
-      console.log('Please enter any text');
-      return;
-    } else {
-      createGallery(data);
-    }
+    createGallery(data);
+  });
+}
+function startPage() {
+  searchFilm = 'red';
+  searchApi.getSearchMovie(searchFilm, page).then(data => {
+    createGallery(data);
   });
 }
