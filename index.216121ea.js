87ec752ca3cd65d44a885e22f1cb790f8f5e61f2
BobryshevAnton <101340267+BobryshevAnton@users.noreply.github.com>
2023-01-10 10:51:46 +0000

    Deploying to gh-pages from @ BobryshevAnton/independent-project@12e52589151bae8f34cefded961ae5f525b28956 🚀

--- index.216121ea.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),closeModalClick:document.querySelector(".backdrop"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.216121ea.js.map
